17c51ded1439c6558a19669b5dcb3575166148f5
Jonathan Bermello <bermelloj21@gmail.com>
2025-12-27 10:36:58 -0600

    111

--- django_portfolio/wsgi.py
+++ django_portfolio/wsgi.py
@@ -1,10 +1,10 @@
 """
-WSGI config for django_portfolio project.
+WSGI config for djangocrud project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
 """
 
 import os
